eac6f183d26bda6e42e86ccfe3338fc6de117f3a
Justin Yue <jyue009@ucr.edu>
2024-12-07 20:39:39 -0800

    move logger code to right file

--- src/logger/logger.py
+++ src/logger/logger.py
@@ -1,2 +1,22 @@
+from types import Dict
+
+import enum
+import wandb
+
+from dataclasses import dataclass
+
+class LoggerMode(enum.Enum):
+    TENSOR_BOARD=0
+    WANDB=1
+
 class Logger:
-    pass
+    def __init__(self, project_name: str, config: dataclass, mode: LoggerMode):
+        if mode == LoggerMode.TENSOR_BOARD:
+            raise NotImplementedError
+        elif mode == LoggerMode.WANDB:
+            self.wandb_run = wandb.init(project=project_name, config=config)
+        else:
+            raise ValueError("Invalid logger mode")
+
+    def log(self, data: Dict, step: int):
+        self.wandb_run.log(data, step=step)
