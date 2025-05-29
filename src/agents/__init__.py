e2c85e329298d25e645a7365e8e24d07abe05581
Justin Yue <jyue009@ucr.edu>
2025-05-29 22:39:16 +0000

    delete old agents code

--- src/agents/__init__.py
+++
@@ -1,1 +0,0 @@
-from .dqn.policy import DQN  # noqa: F401
--- src/agents/dqn/policy.py
+++
@@ -1,8 +0,0 @@
-from src.agents.base_policy import BasePolicy 
-
-class DQN(BasePolicy):
-    def __init__(self):
-        pass
-
-    def make_train(self):
-        return super().make_train()
